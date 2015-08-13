d8be17d06365c70b017096a88c5d443f51a024b3
Siddharth Khialani <siddharthkhialani@Siddharths-MacBook-Air.local>
2015-08-13 00:11:54 -0400

    spring boot app

---
+++ src/main/java/MyCollections/MyCollectionsApplication.java
@@ -0,0 +1,12 @@
+package MyCollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCollectionsApplication.class, args);
+    }
+}
---
+++ src/test/java/MyCollections/MyCollectionsApplicationTests.java
@@ -0,0 +1,18 @@
+package MyCollections;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MyCollectionsApplication.class)
+@WebAppConfiguration
+public class MyCollectionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
